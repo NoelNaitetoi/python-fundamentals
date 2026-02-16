d2d67df41820db57a081e65c06c89c1657e8d962
Naitetoi639 <noelnaitetoi@gmail.com>
2026-02-16 16:26:50 +0300

    modules in python

---
+++ module.py
@@ -0,0 +1,9 @@
+#module is any python file that has a function
+import functions,loops,math
+functions.add(200,340)
+functions.add(20,340)
+functions.school()
+
+
+output=math.sqrt(144)
+print(output)
