d5e2841628278f2768e6e45128a34c9db041dfed
Naitetoi639 <noelnaitetoi@gmail.com>
2026-02-16 15:26:16 +0300

    added loops

---
+++ loops.py
@@ -0,0 +1,18 @@
+#while loop
+#prints only when condition is true
+number=1
+while  number<=5:
+    print(number)
+    number +=1
+
+print()
+count=105
+while count>=100:
+    print("Number is ",count)
+    count-=1
+print()
+#for loop
+for num in range(1,6,2):
+    print("number:",num)
+#break and continue statements
+
