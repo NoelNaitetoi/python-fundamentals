da987253b3c6dab038757413eb02f69d0beed5db
Naitetoi639 <noelnaitetoi@gmail.com>
2026-02-16 15:55:00 +0300

    functions in python

--- functions.py
+++ functions.py
@@ -1,6 +1,20 @@
-def addition (a,b):
-    result=a+b
-    print("The sum is ",result)
-    return result
-a=addition(1,2)
-addition(3,4)
+#function/method -a block of code  used to perform a specific task
+#there are to types of functions
+#1.standard inbuilt functions-already exist eg max
+y=max(45,56,78,97,900,768,4)
+print("The maximum number is :",y)
+x=min(56,67,67,89,45,3,45,78,900)
+print("The smallest number is :",x)
+#user defined function-created by user
+def school ():
+    print("emobilis")
+
+school()
+print()
+
+def add ():
+    num1=20
+    num2=30
+    print("The sum is :",num1+num2)
+add()
+
